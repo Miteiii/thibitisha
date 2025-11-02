f49a8ed765a559efd478d676c00b609727a62cae
Miteiii <chelangat.mitei@stratmore.edu>
2025-11-02 22:13:34 +0300

    first commit

---
+++ app/Http/Controllers/TestRoleController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTestRoleRequest;
+use App\Http\Requests\UpdateTestRoleRequest;
+use App\Models\TestRole;
+
+class TestRoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTestRoleRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TestRole $testRole)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TestRole $testRole)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTestRoleRequest $request, TestRole $testRole)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TestRole $testRole)
+    {
+        //
+    }
+}
