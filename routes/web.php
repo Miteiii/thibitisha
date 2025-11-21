cfaa97e142afdfc4824d4b0e064be73d35410d1b
Miteiii <chelangat.mitei@strathmore.edu>
2025-11-21 12:13:26 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test/home', function () {
+    return view('test.home');
+});
+
+Route::get('test/about', function () {
+    return view('test.about');
+});
+
+Route::get('test/contact', function () {
+    return view('test.contact');
+});
+
+Route::get('roles', function () {
+    return view('roles.index');
+});
